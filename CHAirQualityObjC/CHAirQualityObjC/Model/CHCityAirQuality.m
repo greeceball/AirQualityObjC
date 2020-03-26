5753c6634a7070becc1378fdccc095e9d9a5f702
Colby Harris <32947799+greeceball@users.noreply.github.com>
2020-03-25 20:17:15 -0600

    Add files via upload

---
+++ CHAirQualityObjC/CHAirQualityObjC/Model/CHCityAirQuality.m
@@ -0,0 +1,13 @@
+//
+//  CHCityAirQuality.m
+//  CHAirQualityObjC
+//
+//  Created by Colby Harris on 3/25/20.
+//  Copyright © 2020 Colby_Harris. All rights reserved.
+//
+
+#import "CHCityAirQuality.h"
+
+@implementation CHCityAirQuality
+
+@end
